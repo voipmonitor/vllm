cc3aefd082bc8da9453c93b525a6733218d58fef
Martin Vit <martin@voipmonitor.org>
2026-05-21 16:53:24 +0000

    moe: record shared expert aux output stream
    (cherry picked from commit 8e46bca85fe59895f5bec23079bb7e123335081b)

--- vllm/model_executor/layers/fused_moe/fused_moe_method_base.py
+++ vllm/model_executor/layers/fused_moe/fused_moe_method_base.py
@@ -48,10 +48,6 @@ class FusedMoEMethodBase(QuantizeMethodBase):
             self.moe_kernel is not None and self.moe_kernel.can_overlap_shared_experts
         )
 
-    @property
-    def supports_shared_experts_aux_stream(self) -> bool:
-        return True
-
     @abstractmethod
     def create_weights(
         self,
