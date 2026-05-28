21801f8d49163fb327502ddbea47edc7080ed463
Martin Vit <martin@voipmonitor.org>
2026-05-28 17:58:30 +0000

    fix: align fused MoE inplace removal

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -50,9 +50,6 @@ from vllm.model_executor.layers.fused_moe.runner.shared_experts import (
 from vllm.model_executor.layers.fused_moe.unquantized_fused_moe_method import (
     UnquantizedFusedMoEMethod,
 )
-from vllm.model_executor.layers.fused_moe.utils import (
-    disable_inplace,
-)
 from vllm.model_executor.layers.quantization.base_config import (
     QuantizationConfig,
 )
@@ -337,7 +334,6 @@ class FusedMoE(PluggableLayer):
             routing_method=self.routing_method_type,
             swiglu_limit=swiglu_limit,
             # TODO: in_dtype == out_dtype?
-            disable_inplace=disable_inplace() or shared_experts is not None,
             layer_name=self.layer_name,
             layer_idx=self.layer_id,
         )
